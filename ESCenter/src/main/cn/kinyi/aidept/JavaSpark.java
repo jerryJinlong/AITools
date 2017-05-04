148c6c01d0b45acc1bc6f172cec4b9f2848ce981
jerryJinlong <184062141@qq.com>
2017-05-04 08:41:48 +0800

    create

--- ESCenter/src/main/cn/kinyi/aidept/JavaSpark.java
+++ ESCenter/src/main/cn/kinyi/aidept/JavaSpark.java
@@ -6,5 +6,6 @@ package cn.kinyi.aidept;
 public class JavaSpark {
     public static void main(String[] args) {
         System.out.println("how are  you !");
+        System.out.println("hello111111111111111111111");
     }
 }
